e6edc380f2377b06f8ff616c9e1c6b5328d033a8
Julio Uribe <uribejulioc@gmail.com>
2023-06-28 17:07:40 -0700

    Finish writing the user controller

--- practice-for-ch-rails-first-routes-long-practice-main/first_routes/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes/app/controllers/users_controller.rb
@@ -1,9 +1,39 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+    # replace the `user_attributes_here` with the actual attribute keys
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    # Get an instance of a user
+    user = User.find_by(id: params[:id])
+    # Render their info
+    render json: user
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.update(user_params)
+    render json: user
+  end
+
+  def delete
+    user = User.find_by(id: params[:id])
+    user.destroy
+    redirect_to users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
